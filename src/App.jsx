b8510ed599ca3fe71d237d50ff26fe3c8ce82e20
Stephen James <78153905+ItsMeSteve88@users.noreply.github.com>
2023-07-26 10:23:59 +0100

    Map added

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,63 @@
 import React from 'react'
-import { createTheme, ThemeProvider, Box } from '@mui/material'
+import { createTheme, ThemeProvider, Grid } from '@mui/material'
 import Header from './components/Header/Header'
 import { CssBaseline } from '@mui/material'
+import List from './components/List/List'
+import Map from './components/Map/Map'
 
 const theme = createTheme({
-   palette: {
-      mode: 'light',
-      primary: {
-        main: '#454545',
-      },
-      secondary: {
-        main: '#454545',
-      },
-      background: {
-        default: '#fbeaab',
-      },
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#454545",
     },
-    typography: {
-      fontFamily: 'Mallanna',
-   },
-   shape: {
-       borderRadius: 10,
-   },
-   breakpoints: {
-      sm: 576,
-      md: 768,
-      lg: 992,
-      xl: 1200,
-   },
-})
+    secondary: {
+      main: "#454545",
+    },
+    background: {
+      default: "#fbeaab",
+    },
+  },
+  typography: {
+    fontFamily: "Mallanna",
+  },
+  shape: {
+    borderRadius: 10,
+  },
+  breakpoints: {
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+  },
+});
 
 const App = () =>
 {
-   return (
-      <ThemeProvider theme={theme}>
-         <CssBaseline />
-         <Box>
-         <Header />
-       </Box>
-     </ThemeProvider>
-   );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} sx={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Map />
+        </Grid>
+      </Grid>
+      
+    </ThemeProvider>
+  );
 }
 
 export default App
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, Rating, useMediaQuery, Box } from '@mui/material'
+import { LocationOnOutlined } from '@mui/icons-material'
 
-const Map = () => {
+
+const Map = () =>
+{
+   const isMobile = useMediaQuery('(min-width: 600px)')
+   const coordinates = { lat: 0, lng: 0 }
   return (
-    <div>Map</div>
+     <Box sx={{
+        height: '85vh',
+         width: '100%',
+       }}>
+        <GoogleMapReact
+           bootstrapURLKeys={{ key: 'AIzaSyAX1LN2B6KphTxSg4Kf4jVKlhe2bLFNNUI'}}
+           defaultCenter={coordinates}
+           center={coordinates}
+           defaultZoom={14}
+           margin={[50, 50, 50, 50]}
+           options={''}
+           onChange={''}
+           onChildClick={''}
+        >
+           
+        </GoogleMapReact>
+    </Box>
   )
 }
 
