1504890a610861599aa0693a22e44cb764a29021
Stephen James <78153905+ItsMeSteve88@users.noreply.github.com>
2023-07-26 09:50:05 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { createTheme, ThemeProvider, Box } from '@mui/material'
+import Header from './components/Header/Header'
+import { CssBaseline } from '@mui/material'
 
-const App = () => {
-  return (
-    <div>hello mui</div>
-  )
+const theme = createTheme({
+   palette: {
+      mode: 'light',
+      primary: {
+        main: '#454545',
+      },
+      secondary: {
+        main: '#454545',
+      },
+      background: {
+        default: '#fbeaab',
+      },
+    },
+    typography: {
+      fontFamily: 'Mallanna',
+   },
+   shape: {
+       borderRadius: 10,
+   },
+   breakpoints: {
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+   },
+})
+
+const App = () =>
+{
+   return (
+      <ThemeProvider theme={theme}>
+         <CssBaseline />
+         <Box>
+         <Header />
+       </Box>
+     </ThemeProvider>
+   );
 }
 
 export default App
